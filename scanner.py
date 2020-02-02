eabb7b6e7310212b73fd73f8764489d6923b12e0
tlbot <28433525+tlb99@users.noreply.github.com>
2020-02-02 12:57:43 -0800

    init push

---
+++ scanner.py
@@ -0,0 +1,40 @@
+#!/bin/python
+
+import sys
+import socket
+from datetime import datetime
+
+# Define our target
+if len(sys.argv) == 2: # TODO add input error handling
+    target = socket.gethostbyname(sys.argv[1]) # Translate hostname to IPv4
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 scanner.py <ip>")
+    sys.exit()
+
+# 
+print("-" * 50)
+print("Scanning target " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(1, 65535): # TODO add threading
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port)) # returns an error indicator if connection was unsuccessful
+        if result == 0:
+            print("Port {} is open".format(port)) 
+        s.close() # close connection
+
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server.")
+    sys.exit()
