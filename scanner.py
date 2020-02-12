b384d3e92a1b986c41deb3fa9444ae788d245f86
tlbot <28433525+tlb99@users.noreply.github.com>
2020-02-12 13:37:18 -0800

    added user input error handling

--- scanner.py
+++ scanner.py
@@ -5,21 +5,25 @@ import socket
 from datetime import datetime
 
 # Define our target
-if len(sys.argv) == 2: # TODO add input error handling
-    target = socket.gethostbyname(sys.argv[1]) # Translate hostname to IPv4
+if len(sys.argv) == 2:
+    try:
+        target = socket.gethostbyname(sys.argv[1]) # Translate hostname to IPv4
+    except socket.error:
+        print("Invalid IP address.")
+        sys.exit()
 else:
     print("Invalid amount of arguments.")
     print("Syntax: python3 scanner.py <ip>")
     sys.exit()
 
-# 
+# Print banner 
 print("-" * 50)
 print("Scanning target " + target)
 print("Time started: " + str(datetime.now()))
 print("-" * 50)
 
 try:
-    for port in range(1, 65535): # TODO add threading
+    for port in range(1, 65535): # TODO add threading support
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((target, port)) # returns an error indicator if connection was unsuccessful
